db099232ad488bf57a374462b012d4858ca45665
UI-Interface <118592575+UI-Interface@users.noreply.github.com>
2022-11-19 15:53:31 -0500

    Create Template.lua

---
+++ Template.lua
@@ -0,0 +1,94 @@
+return Template = {
+	["SwimAnimation"]  =  0,
+	["RunAnimation"]   =  0,
+	["ClimbAnimation"] =  0,
+	["JumpAnimation"]  =  0,
+	["IdleAnimation"]  =  0,
+	["WalkAnimation"]  =  0,
+	["FallAnimation"]  =  0,
+
+	["Shirt"] = 0,
+	["Pants"] = 0,
+	["Face"]  = 0,
+
+	["FrontAccessory"]     =  {},
+	["FaceAccessory"]      =  {},
+	["HairAccessory"]      =  {},
+	["HeadAccessory"]      =  {},
+	["NeckAccessory"]      =  {},
+	["ShouldersAccessory"] =  {}
+	["WaistAccessory"]     =  {}
+	
+	["Head"]     =  0,
+	["Torso"]    =  0,
+	["RightArm"] =  0,
+	["LeftArm"]  =  0,
+	["LeftLeg"]  =  0,
+	["RightLeg"] =  0,
+
+	["HeadColor"]    = {
+		[1] = "0",
+		[2] = "0",
+		[3] = "0"
+	},
+	["TorsoColor"]   = {
+		[1] = "0",
+		[2] = "0",
+		[3] = "0"
+	},
+	["LeftArmColor"] = {
+		[1] = "0",
+		[2] = "0",
+		[3] = "0"
+	},
+	["RightArmColor"] = {
+		[1] = "0",
+		[2] = "0",
+		[3] = "0"
+	},
+	 ["LeftLegColor"] = {
+		[1] = "0",
+		[2] = "0",
+		[3] = "0"
+	},
+	["RightLegColor"] = {
+		[1] = "0",
+		[2] = "0",
+		[3] = "0"
+	},
+
+	["HeightScale"]     =  0,
+	["DepthScale"]      =  0,
+	["ProportionScale"] =  0,
+	["HeadScale"]       =  0,
+	["WidthScale"]      =  0,
+	["BodyTypeScale"]   =  0,
+
+	["Emotes"] = {
+		["1"] = {
+			[1] = 0
+		},
+		["2"] = {
+			[1] = 0
+		},
+		["3"] = {
+			[1] = 0
+		},
+		["4"] = {
+			[1] = 0
+		},
+		["5"] = {
+			[1] = 0
+		},
+		["6"] = {
+			[1] = 0
+		},
+		["7"] = {
+			[1] = 0
+		},
+		["8"] = {
+			[1] = 0
+		}
+	},
+}
+
